2e9daca7d818f07634eccd66282c62a8ddd0d737
Shahzad Khan <joykhan78600@gmail.com>
2026-08-25 18:17:35 +0530

    refactor: compose mockup-aligned homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
 import { Hero } from "@/components/home/hero";
 import { HomeSections } from "@/components/home/home-sections";
-import { TripSearch } from "@/components/home/trip-search";
 
 export default function HomePage() {
   return (
@@ -10,7 +9,6 @@ export default function HomePage() {
       <Header />
       <main>
         <Hero />
-        <TripSearch />
         <HomeSections />
       </main>
       <Footer />
