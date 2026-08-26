29edfae2bc21937f9c18534a091f20a8a20b5175
Shahzad Khan <joykhan78600@gmail.com>
2026-08-26 14:47:29 +0530

    Add destinations admin page

---
+++ app/admin/(protected)/destinations/page.tsx
@@ -0,0 +1,3 @@
+import { DestinationManager } from "@/components/admin/destination-manager";
+export const dynamic="force-dynamic";
+export default function DestinationsAdminPage(){return <DestinationManager/>}
