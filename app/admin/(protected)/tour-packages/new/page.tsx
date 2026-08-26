656d4c0ed51d949285819b3e2dc5e4300ede630f
Shahzad Khan <joykhan78600@gmail.com>
2026-08-26 14:20:24 +0530

    Add create tour package page

---
+++ app/admin/(protected)/tour-packages/new/page.tsx
@@ -0,0 +1,7 @@
+import { TourPackageForm } from "@/components/admin/tour-package-form";
+
+export const dynamic = "force-dynamic";
+
+export default function NewTourPackagePage() {
+  return <TourPackageForm />;
+}
