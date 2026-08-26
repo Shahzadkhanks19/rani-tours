44d51a9c728841652d1eb3cea024102ba4acee6c
Shahzad Khan <joykhan78600@gmail.com>
2026-08-26 14:20:18 +0530

    Add tour packages admin page

---
+++ app/admin/(protected)/tour-packages/page.tsx
@@ -0,0 +1,7 @@
+import { TourPackageManager } from "@/components/admin/tour-package-manager";
+
+export const dynamic = "force-dynamic";
+
+export default function TourPackagesAdminPage() {
+  return <TourPackageManager />;
+}
