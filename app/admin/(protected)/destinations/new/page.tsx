9c052a513c434515ada38c66002d748e941e5f07
Shahzad Khan <joykhan78600@gmail.com>
2026-08-26 14:47:36 +0530

    Add new destination admin page

---
+++ app/admin/(protected)/destinations/new/page.tsx
@@ -0,0 +1,3 @@
+import { DestinationForm } from "@/components/admin/destination-form";
+export const dynamic="force-dynamic";
+export default function NewDestinationPage(){return <DestinationForm/>}
