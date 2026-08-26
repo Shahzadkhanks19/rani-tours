0f82f3f47ddd81dc275a8bfee9c0afc9ec4afbb9
Shahzad Khan <joykhan78600@gmail.com>
2026-08-26 14:36:37 +0530

    Add taxi services admin page

---
+++ app/admin/(protected)/taxi-services/page.tsx
@@ -0,0 +1,7 @@
+import { TaxiServiceManager } from "@/components/admin/taxi-service-manager";
+
+export const dynamic = "force-dynamic";
+
+export default function TaxiServicesAdminPage(){
+  return <TaxiServiceManager/>;
+}
